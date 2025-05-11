216c57f1e7b02e5be02691cbc3fa2692c23cac12
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-05-11 11:50:38 +0300

    Add project files.

---
+++ CyrusTask/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CyrusTask.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ CyrusTask/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace CyrusTask.Models
+{
+    public class Project : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+
+
+    }
+}
